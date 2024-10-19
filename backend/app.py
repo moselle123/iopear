65e3673e61b4b68a4858f59037c053bf57164301
moselle123 <m.pattison.christie@gmail.com>
2024-10-19 14:16:00 +0100

    create basic structure for backend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request, url_for, redirect
+from pymongo import MongoClient
+
 app = Flask(__name__, static_folder='templates/assets')
+
+client = MongoClient('localhost', 27017)
+db = client.io_pear
 
 if __name__ == "main":
         app.run(debug=True)
 
+# host frontend
 @app.route('/', methods=('GET', 'POST'))
 def index():
     return render_template('index.html')
+
