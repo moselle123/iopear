3de0ffeac8ae150f2d2745e2b901982c37e40871
moselle123 <m.pattison.christie@gmail.com>
2024-10-19 11:22:20 +0100

    rename frontend directory from src to vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	root: 'src',
+	root: 'vite',
 	publicDir: 'resources',
 	build: {
 		outDir:'../templates',
