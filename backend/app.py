d6bdc08e2b54ea9c11b3046b50ae9e2d0e694724
moselle123 <m.pattison.christie@gmail.com>
2024-10-27 00:17:13 +0100

    add composer file to run mongodb from docker

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
+import os
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from services.i2c_manager import I2CManager
 app = Flask(__name__, static_folder='templates/assets')
 
-client = MongoClient('localhost', 27017)
+mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/iopear_db")
+client = MongoClient(mongo_uri)
 db = client.io_pear
 
 if __name__ == "main":
-		app.run(debug=True)
+	app.run(host="0.0.0.0", debug=True)
 
 # host frontend
 @app.route('/', methods=('GET', 'POST'))
