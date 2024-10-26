3373d249f13f8f4441c9422adc8e1d2e75901ad1
moselle123 <m.pattison.christie@gmail.com>
2024-10-26 01:34:23 +0100

    fix test to scan multiplexor instead of i2c bus

--- backend/services/i2c_manager.py
+++ backend/services/i2c_manager.py
@@ -1,32 +1,46 @@
 import board
 import busio
-from sensors import SHT31
-from sensors import TSL2561
+import adafruit_tca9548a
+from sensors import SHT31, TSL2561
 
 i2c = busio.I2C(board.SCL, board.SDA)
-
-while not i2c.try_lock():
-	pass
+TCA9548A_ADDRESS = 0x70
 
 try:
-	devices = i2c.scan()
+    	multiplexor = adafruit_tca9548a.TCA9548A(i2c, address=TCA9548A_ADDRESS)
+except ValueError:
+	print("Could not find TCA9548A at address 0x" + TCA9548A_ADDRESS)
+	exit()
 
-	if devices:
-		for address in devices:
-			if address == 0x44:
-				tempSensor = SHT31(i2c)
-				humiditySensor = tempSensor
-				print(tempSensor.getTemperature())
-				tempSensor.getHumidity()
-				print("SHT31 Sensor found at i2c address 0x44")
-				
-			elif address == 0x70:
-				print("Found multiplexor")
-				
-			elif address == 0x29:
-				tsl2561 = TSL2561(i2c)
-				print("TSL2561 Sensor found at i2c address 0x29")
-	else:
-		print("No I2C devices found")
-finally:
-	i2c.unlock()
+for channel in range(8):
+	print("\nScanning devices on channel" + channel + "...")
+	i2c = multiplexor[channel]
+
+	while not i2c.try_lock():
+		pass
+
+	try:
+		devices = i2c.scan()
+		
+		if devices:
+			for address in devices:
+				if address == 0x44:
+					tempSensor = SHT31(i2c)
+					print("SHT31 Sensor found at address 0x44")
+					print("Temperature:", tempSensor.getTemperature())
+					print("Humidity:", tempSensor.getHumidity())
+
+				elif address == 0x29:
+					tsl2561 = TSL2561(i2c)
+					print("TSL2561 Sensor found at i2c address 0x29")
+					print("Light Level:", tsl2561.getLightIntensity()) 
+
+				else:
+					print("Unknown device found at address 0x" + address)
+		else:
+			print("No I2C devices found on this channel.")
+
+	finally:
+		i2c.unlock()
+
+print("Finished.")
