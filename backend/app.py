4a37b77e9ff2fd90097383b7d7ebe78c588ff218
Moselle <mwpc1@student.le.ac.uk>
2024-11-01 15:40:27 +0000

    flask app running in docker without i2c manager

--- backend/app.py
+++ backend/app.py
@@ -2,18 +2,20 @@ import os
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from services.i2c_manager import I2CManager
+
 app = Flask(__name__, static_folder='templates/assets')
 
 mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/iopear_db")
 client = MongoClient(mongo_uri)
 db = client.io_pear
 
-if __name__ == "main":
-	app.run(host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=5000, debug=True)
 
 # host frontend
 @app.route('/', methods=('GET', 'POST'))
 def index():
 	return render_template('index.html')
 
-i2c_manager = I2CManager()
+#i2c_manager = I2CManager()
+print("scanning done")
--- backend/services/i2c_manager.py
+++ backend/services/i2c_manager.py
@@ -2,6 +2,7 @@ import board
 import busio
 import adafruit_sht31d
 import adafruit_tsl2561
+from adafruit_seesaw.seesaw import Seesaw
 
 class I2CManager:
 	def __init__(self):
@@ -9,9 +10,9 @@ class I2CManager:
 		self.sht = None
 		self.tsl = None
 		
-		self._initialize_sensors()
+		self._initialise_sensors()
 
-	def _initialise_devices(self):
+	def _initialise_sensors(self):
 		print("Scanning I2C bus for devices...")
 		while not self.i2c.try_lock():
 			pass
@@ -19,6 +20,7 @@ class I2CManager:
 			devices = self.i2c.scan()
 			if devices:
 				for address in devices:
+					try:
 					print(f"Device found at address 0x{address:02x}")
 					if address == 0x44:
 						self.sht = adafruit_sht31d.SHT31D(self.i2c)
@@ -26,6 +28,9 @@ class I2CManager:
 					elif address == 0x29:
 						self.tsl = adafruit_tsl2561.TSL2561(self.i2c)
 						print("TSL2561 Sensor initialized at address 0x29.")
+					elif address == 0x36:
+						self.ss = Seesaw(self.i2c, addr=0x36)
+						print("Soil Moisture Sensor initialized at address 0x36.")
 			else:
 				print("No I2C devices found.")
 		finally:
@@ -36,6 +41,12 @@ class I2CManager:
 
 	def get_humidity(self):
 		return self.sht.relative_humidity if self.sht else None
+	
+	def get_soil_moisture(self):
+		return self.ss.moisture_read() if self.ss else None
 
+	def get_soil_temperature(self):
+		return self.ss.get_temp() if self.ss else None
+	
 	def get_light_level(self):
 		return self.tsl.lux if self.tsl else None
