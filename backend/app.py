92326046fe2f0970a9f4ea00774a70c0c6eda30e
Moselle <mwpc1@student.le.ac.uk>
2024-11-08 15:07:36 +0000

    create endpoint /get_plant

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,7 @@ def signal_handler(sig, frame):
 	i2c_manager.stop_reading()
 	logger.warning("I2C manager stopped. Exiting application.")
 	exit(0)
-	
+
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 #i2c_manager = I2CManager(db)
@@ -35,7 +35,7 @@ app = Flask(__name__, static_folder='templates/assets')
 CORS(app, origins=["http://localhost:5173", "http://192.168.4.116:5173", "100.103.241.41:5173"])
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)
-	
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
 	return render_template('index.html')
@@ -47,14 +47,14 @@ def get_sensor_data():
 	except Exception as e:
 		logger.error(f"Error getting sensor data: {e}")
 		return {"error": "Failed to retrieve sensor data"}, 500
-	
+
 @app.route('/get_plant_types', methods=['GET'])
 def get_plant_types():
 	try:
 		plant_types = list(db["plant_types"].find({}))
 		for plant in plant_types:
 			plant["_id"] = str(plant["_id"])
-		
+
 		return jsonify(plant_types)
 	except Exception as e:
 		logger.error(f"Error getting plant types data: {e}")
@@ -66,9 +66,20 @@ def get_sensors():
 		sensors = list(db["sensors"].find({}))
 		for sensor in sensors:
 			sensor["_id"] = str(sensor["_id"])
-		
+
 		return jsonify(sensors)
 	except Exception as e:
 		logger.error(f"Error getting sensors collection: {e}")
 		return {"error": "Failed to retrieve sensors collection"}, 500
 
+@app.route('/get_plant', methods=['GET'])
+def get_plant():
+	try:
+		if "plant" not in db.list_collection_names():
+			return jsonify({})
+
+		plant = list(db["plant"].find({}))
+		return jsonify(plant)
+	except Exception as e:
+		logger.error(f"Error getting plant collection: {e}")
+		return {"error": "Failed to retrieve plant collection"}, 500
