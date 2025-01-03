d174a314585e420f90d9fc16f667faf728349480
Moselle <mwpc1@student.le.ac.uk>
2025-01-03 15:06:55 +0000

    refactor plant_type model

--- backend/app/models/plant_type.py
+++ backend/app/models/plant_type.py
@@ -2,32 +2,32 @@ from bson import ObjectId
 from flask import current_app
 
 class PlantType:
-	def __init__(self, name, nicknames, thresholds, description):
-		self.name = name
-		self.nicknames = nicknames
-		self.thresholds = thresholds
-		self.description = description
-
-	def to_dict(self):
-		return {
-			"name": self.name,
-			"nicknames": self.nicknames,
-			"thresholds": self.thresholds,
-			"description": self.description,
+	@staticmethod
+	def create(name, nicknames, thresholds, description):
+		plant_data = {
+			"name": name,
+			"nicknames": nicknames,
+			"thresholds": thresholds,
+			"description": description,
 		}
-
-	@classmethod
-	def create(cls, name, nicknames, thresholds, description):
-		plant = cls(name, nicknames, thresholds, description)
-		result = current_app.config['DB']["plant_type"].insert_one(plant.to_dict())
-		return result.inserted_id
-
-	@classmethod
-	def get_by_id(cls, plant_type_id):
-		plant_data = current_app.config['DB']["plant_type"].find_one({"_id": ObjectId(plant_type_id)})
+		result = current_app.config['DB']["plant_type"].insert_one(plant_data)
+		plant_data["_id"] = str(result.inserted_id)
 		return plant_data
 
-	@classmethod
-	def delete(cls, plant_type_id):
+	@staticmethod
+	def get_by_id(plant_type_id):
+		result = current_app.config['DB']["plant_type"].find_one({"_id": ObjectId(plant_type_id)})
+		if not result:
+                        return None
+                return {
+                        "_id": str(result["id"]),
+                        "name": result["name"],
+                        "nicknames": result["nicknames"],
+                        "thresholds": result["thresholds"],
+                        "description": result["description"],
+                }
+
+	@staticmethod
+	def delete(plant_type_id):
 		result = current_app.config['DB']["plant_type"].delete_one({"_id": ObjectId(plant_type_id)})
 		return result.deleted_count
