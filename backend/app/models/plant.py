11544fdf4f227950b140682ee6fa2489d1ba4a03
moselle123 <m.pattison.christie@gmail.com>
2025-01-06 20:49:06 +0000

    clean up plant model

--- backend/app/models/plant.py
+++ backend/app/models/plant.py
@@ -2,14 +2,8 @@ from bson import ObjectId
 from flask import current_app
 
 class Plant:
-	def __init__(self, name, plant_type_id, thresholds, sensors):
-		self.name = name
-		self.plant_type_id = plant_type_id
-		self.thresholds = thresholds
-		self.sensors = sensors
-
-	@classmethod
-	def to_dict(cls):
+	@staticmethod
+	def to_dict():
 		plant = current_app.config['DB']["plant"].find_one({})
 		if not plant:
 			return {}
@@ -26,19 +20,14 @@ class Plant:
 			"sensors": plant['sensors'],
 		}
 
-	@classmethod
-	def create(cls, name, plant_type_id, thresholds):
+	@staticmethod
+	def create(name, plant_type_id, thresholds):
 		sensors = list(current_app.config['DB']["sensors"].find({}))
 		plant_type_id = ObjectId(plant_type_id)
 		plant = cls(name, plant_type_id, thresholds, sensors)
 
 		result = current_app.config['DB']["plant"].insert_one({"name": name, "plant_type_id": ObjectId(plant_type_id), "thresholds": thresholds, "sensors": sensors})
 		return result.inserted_id
-
-	@classmethod
-	def get_by_id(cls, plant_id):
-		plant_data = current_app.config['DB']["plant"].find_one({"_id": ObjectId(plant_id)})
-		return plant_data
 
 	@classmethod
 	def delete(cls, plant_id):
