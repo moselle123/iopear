34dc06bfcd2128a9c00c254aee0cf76ec8d4dfa4
moselle123 <m.pattison.christie@gmail.com>
2025-01-15 14:07:34 +0000

    change isActive flag on events to isEnabled

--- backend/app/routes/event.py
+++ backend/app/routes/event.py
@@ -24,7 +24,7 @@ def get_events():
 def create_event():
 	data = request.json
 	try:
-		Event.create(data["measurement"], data["condition"], data["threshold"], data["isActive"])
+		Event.create(data["measurement"], data["condition"], data["threshold"], data["isEnabled"])
 		return {"message": "Event created"}, 201
 	except Exception as e:
 		logger.error(f"Error getting updating event: {e}")
@@ -34,7 +34,7 @@ def create_event():
 def update_event(event_id):
 	data = request.json
 	try:
-		Event.update(event_id, data["measurement"], data["condition"], data["threshold"], data["isActive"])
+		Event.update(event_id, data["measurement"], data["condition"], data["threshold"], data["isEnabled"])
 		return {"message": "Event updated"}, 200
 	except Exception as e:
 		logger.error(f"Error getting updating event: {e}")
