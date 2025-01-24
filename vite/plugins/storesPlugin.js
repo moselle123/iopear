045dde5d4f799918f21bde331e046be94e0d7d68
moselle123 <m.pattison.christie@gmail.com>
2025-01-24 12:58:14 +0000

    register action and actuator store in stores plugin

--- vite/plugins/storesPlugin.js
+++ vite/plugins/storesPlugin.js
@@ -1,6 +1,8 @@
 import { usePlantStore } from '../stores/usePlantStore';
 import { useSensorStore } from '../stores/useSensorStore';
 import { useEventStore } from '../stores/useEventStore';
+import { useActionStore } from '../stores/useActionStore';
+import { useActuatorStore } from '../stores/useActuatorStore';
 
 export default {
 	install(app) {
@@ -8,6 +10,8 @@ export default {
 			plantStore: usePlantStore(),
 			sensorStore: useSensorStore(),
 			eventStore: useEventStore(),
+			actionStore: useActionStore(),
+			actuatorStore: useActuatorStore(),
 		};
 	}
 };
