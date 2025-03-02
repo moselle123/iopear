2b77175302872f4d01759f4e34568ef0c90dd932
moselle123 <m.pattison.christie@gmail.com>
2025-03-02 13:21:23 +0000

    remove unnessary action endpoints

--- backend/app/routes/action.py
+++ backend/app/routes/action.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, jsonify, request
 import logging
 from app.models import Action
-from app.models import Notification
 from app.services import ActionManager
+from flask_socketio import emit
+from app import socketio
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -23,51 +24,3 @@ def get_actions():
 		logger.error(f"Error getting actions data: {e}")
 		return {"error": "Failed to retrieve actions"}, 500
 
-@action_bp.route('/create_action', methods=['POST'])
-def create_action():
-	data = request.json
-	try:
-		Action.create(data["name"], data["actuator_id"], data["actuator_state"])
-		ActionManager.update_action_list()
-		return {"message": "action created"}, 201
-
-	except Exception as e:
-		logger.error(f"Error updating action: {e}")
-		return {"error": "Failed to create action"}, 500
-
-@action_bp.route('/update_action/<action_id>', methods=['PUT'])
-def update_action(action_id):
-	data = request.json
-	try:
-		message = Action.update(action_id, data)
-		if not message["success"]:
-			return {"message": message}, 404
-
-		ActionManager.update_action_list()
-		return {"message": message}, 200
-
-	except Exception as e:
-		logger.error(f"Error updating action: {e}")
-		return {"error": "Failed to update action"}, 500
-
-@action_bp.route('/delete_action/<action_id>', methods=['DELETE'])
-def delete_action(action_id):
-	try:
-		Action.delete(action_id)
-		Notification.delete_notifications_by_entity(action_id)
-		ActionManager.update_action_list()
-		return {"message": "action deleted"}, 200
-
-	except Exception as e:
-		logger.error(f"Error deleting action: {e}")
-		return {"error": "Failed to delete action"}, 500
-
-@action_bp.route('/trigger_action/<action_id>', methods=['POST'])
-def trigger_action(action_id):
-	try:
-		ActionManager.trigger_action(action_id)
-		return {"message": "Action triggered"}, 200
-
-	except Exception as e:
-		logger.error(f"Error triggering action: {e}")
-		return {"error": "Failed to trigger action"}, 500
