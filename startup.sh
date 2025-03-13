08dce33772b834842d18b11602ff8f9cc8e9773a
moselle123 <m.pattison.christie@gmail.com>
2025-03-13 18:40:21 +0000

    start ap mode within startup script

--- startup.sh
+++ startup.sh
@@ -9,36 +9,39 @@ cd "$(dirname "$0")" || { echo "❌ Error: Could not navigate to project folder!
 echo "🌐 Checking network status..."
 sleep 5
 if ! ip -4 addr show wlan0 | grep -q "inet "; then
-    echo "❌ No network detected! Starting captive portal..."
+	echo "❌ No network detected! Starting captive portal..."
 
-    echo "🚀 Building Captive Portal..."
-    docker build -t captive_portal ./captive_portal
+	sudo systemctl stop wpa_supplicant NetworkManager
+    	sudo systemctl restart hostapd dnsmasq
 
-    echo "🚀 Starting Captive Portal..."
-    docker run -d --name captive_portal_container --network host captive_portal
+	echo "🚀 Building Captive Portal..."
+	docker build -t captive_portal ./captive_portal
 
-    while ! ip -4 addr show wlan0 | grep -q "inet "; do
-        echo "🔄 Waiting for network connection..."
-        sleep 10
-    done
+	echo "🚀 Starting Captive Portal..."
+	docker run -d --name captive_portal_container --network host captive_portal
 
-    echo "✅ Network connected! Stopping Captive Portal..."
-    docker stop captive_portal_container
-    docker rm captive_portal_container
+	while ! ip -4 addr show wlan0 | grep -q "inet "; do
+		echo "🔄 Waiting for network connection..."
+		sleep 10
+	done
 
-    echo "🚀 Starting Main Application..."
-    docker compose -f docker-compose.dev.yml up -d
+	echo "✅ Network connected! Stopping Captive Portal..."
+	docker stop captive_portal_container
+	docker rm captive_portal_container
+
+	echo "🚀 Starting Main Application..."
+	docker compose -f docker-compose.dev.yml up -d
 else
-    echo "✅ Network detected! Ensuring only Main Application is running..."
+	echo "✅ Network detected! Ensuring only Main Application is running..."
 
-    if docker ps | grep -q "captive_portal_container"; then
-        echo "🛑 Stopping Captive Portal..."
-        docker stop captive_portal_container
-        docker rm captive_portal_container
-    fi
+	if docker ps | grep -q "captive_portal_container"; then
+		echo "🛑 Stopping Captive Portal..."
+		docker stop captive_portal_container
+		docker rm captive_portal_container
+	fi
 
-    echo "🚀 Starting Main Application..."
-    docker compose -f docker-compose.dev.yml up -d
+	echo "🚀 Starting Main Application..."
+	docker compose -f docker-compose.dev.yml up -d
 fi
 
 echo "✅ Setup complete!"
