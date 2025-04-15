7602dd7947900cc7d548e972985beaf68c61b57e
moselle123 <m.pattison.christie@gmail.com>
2025-04-15 16:54:38 +0100

    update event endpoints

--- backend/app/routes/event.py
+++ backend/app/routes/event.py
@@ -3,6 +3,7 @@ import logging
 from app.models import Event
 from app.models import Notification
 from app.services import EventManager
+import bson
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -24,6 +25,8 @@ def get_events():
 @event_bp.route('/create_event', methods=['POST'])
 def create_event():
 	data = request.json
+	if not data:
+		return {"error": "Failed to create event, no valid data was given."}, 400
 	try:
 		event_id = Event.create(name=data["name"], conditions=data["conditions"], logic=data["logic"], actions=data["actions"], scheduled_time=data["scheduled_time"], is_enabled=data["is_enabled"])
 		EventManager.update_event_list(event_id)
@@ -34,6 +37,8 @@ def create_event():
 
 @event_bp.route('/update_event/<event_id>', methods=['PUT'])
 def update_event(event_id):
+	if not bson.ObjectId.is_valid(event_id):
+		return {"message": "ID entered is not a valid ObjectId, it must be a 12-byte input or a 24-character hex string."}, 400
 	data = request.json
 	try:
 		message = Event.update(event_id, data)
@@ -48,7 +53,9 @@ def update_event(event_id):
 @event_bp.route('/delete_event/<event_id>', methods=['DELETE'])
 def delete_event(event_id):
 	try:
-		Event.delete(event_id)
+		event = Event.delete(event_id)
+		if not event:
+			return {"message": "No event found with the given ID."}, 404
 		Notification.delete_notifications_by_entity(event_id)
 		return {"message": "Event deleted"}, 200
 	except Exception as e:
